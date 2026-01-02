478079b09f32309e9155e4849009df1f162baf4f
Jianlin Li <jianlin.li@uwaterloo.ca>
2026-01-02 15:20:14 -0500

    save

--- jax-status.nix
+++ jax-status.nix
@@ -1,61 +1,29 @@
 { lib
-, python312
-, stdenv
-, cudaPackages ? null
-, makeWrapper
+, python3
+, python3Packages
+, jaxlib ? python3Packages.jaxlib
 }:
 
-let
-  # Use CUDA-enabled JAX on Linux
-  jaxlibCuda = if stdenv.isLinux && lib.hasAttr "jaxlibWithCuda" python312.pkgs then
-    python312.pkgs.jaxlibWithCuda
-  else if stdenv.isLinux && cudaPackages != null then
-    python312.pkgs.jaxlib.override {
-      cudaSupport = true;
-      cudaPackages = cudaPackages;
-    }
-  else
-    python312.pkgs.jaxlib;
+python3Packages.buildPythonPackage rec {
+  pname = "jax-status";
+  version = "0.1.0";
+  format = "pyproject";
 
-  jax-status-pkg = python312.pkgs.buildPythonPackage rec {
-    pname = "jax-status";
-    version = "0.1.0";
-    format = "pyproject";
+  src = ./.;
 
-    src = ./.;
+  nativeBuildInputs = with python3Packages; [
+    setuptools
+    wheel
+  ];
 
-    nativeBuildInputs = with python312.pkgs; [
-      setuptools
-      wheel
-    ];
+  propagatedBuildInputs = [
+    python3Packages.jax
+    jaxlib
+  ];
 
-    propagatedBuildInputs = [
-      python312.pkgs.jax
-      jaxlibCuda
-    ];
-
-    meta = with lib; {
-      description = "A minimal Python project to inspect JAX runtime status";
-      license = licenses.mit;
-      maintainers = [ ];
-    };
+  meta = with lib; {
+    description = "A minimal Python project to inspect JAX runtime status";
+    license = licenses.mit;
+    maintainers = [ ];
   };
-in
-
-# Wrap the binary to set CUDA_PATH on Linux when cudaPackages is available
-if stdenv.isLinux && cudaPackages != null then
-  stdenv.mkDerivation {
-    pname = "jax-status";
-    version = jax-status-pkg.version;
-    inherit (jax-status-pkg) meta;
-
-    buildInputs = [ makeWrapper ];
-
-    buildCommand = ''
-      mkdir -p $out/bin
-      makeWrapper ${jax-status-pkg}/bin/jax-status $out/bin/jax-status \
-        --set CUDA_PATH ${cudaPackages.cudatoolkit}
-    '';
-  }
-else
-  jax-status-pkg
+}
